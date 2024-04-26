ec9ddf16df387d943f5b918163fce3350855274e
abhijeet-rath <167978782+abhijeet-rath@users.noreply.github.com>
2024-04-26 18:47:34 +0000

    hi

---
+++ 1_basics/4_variables.js
@@ -0,0 +1,14 @@
+const accountID = 2345678
+let accountEMAIL = "abhi@google.com"
+var accountPassword = "123456"
+accountCity = "Bilaspur"
+
+// accountID = 8654859 //This is already assigned under const.
+accountEMAIL = "abhijeet@google.com"
+accountPassword = "82828282"
+accountCity = "Bangalore"
+console.log(accountEMAIL);
+console.table([accountEMAIL, accountPassword, accountCity])
+/*
+Prefer not to use var because of issue in block scope and functional scope.
+*/
