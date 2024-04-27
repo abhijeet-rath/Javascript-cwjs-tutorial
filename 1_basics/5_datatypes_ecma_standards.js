2cc2ae8ae64106a1b8b030ca92459a1f32c908c1
abhijeet-rath <167978782+abhijeet-rath@users.noreply.github.com>
2024-04-27 06:33:50 +0000

    datatypes

---
+++ 1_basics/5_datatypes_ecma_standards.js
@@ -0,0 +1,34 @@
+"use strict" // trat all js code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
++ 3);  // readability should be high
+
+console.log("Abhijeet");
+
+let name = "Abhijeet"
+let age = 27
+let isLoggedIn = false
+let state;
+
+
+// mdn documentation recommended
+// js original documentation - TC39
+
+
+
+//number => ranges to 2 to the power 53
+//bigint => for very large no.s like in trading,stockmarket,fb,reddit..
+//strings => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object 
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
