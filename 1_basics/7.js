75f6cae31eb4f2523f0a431ded7c7fca39608e63
abhijeet-rath <167978782+abhijeet-rath@users.noreply.github.com>
2024-04-27 08:01:51 +0000

    string to number conversion

---
+++ 1_basics/7.js
@@ -0,0 +1,32 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Abhijeet"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1  + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);  //no conversion coz str is 1st
+console.log(1 + 2 + "2");  //first conversion then str
+console.log(true);
+console.log(+true);  // true+ is not possible
+console.log(+"");  //not a good practice
+let num1, num2, num3
+num1 = num2 = num3 = 2+2  // low readability
+let gameCounter = 100
+gameCounter++;  //prefix & postfix operator => increamental operator
+console.log(gameCounter);
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
