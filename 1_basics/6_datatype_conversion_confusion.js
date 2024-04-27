59c072e93046560aafecb4a8a64823e1f1544c75
abhijeet-rath <167978782+abhijeet-rath@users.noreply.github.com>
2024-04-27 07:11:21 +0000

    conversion of datatype

--- 1_basics/6_datatype_conversion_confusion.js
+++ 1_basics/6_datatype_conversion_confusion.js
@@ -1,6 +1,9 @@
-let score = 33
-//let score = 33abc
-
+//let score = 33
+let score = "33abc"
+//let score = null
+//let score = undefined
+//let score = true
+//let score = "Abhijeet"
 
 
 console.log(typeof score);
@@ -9,3 +12,20 @@ console.log(typeof(score));
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; flase => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+//"Abhijeet" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
